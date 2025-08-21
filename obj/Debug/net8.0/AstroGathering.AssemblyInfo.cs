4c4014adba5c4468ea4a8858b91daee8cd2d4211
massirr <124568542+massirr@users.noreply.github.com>
2025-08-21 15:38:42 +0200

    cleaning

--- obj/Debug/net8.0/AstroGathering.AssemblyInfo.cs
+++ obj/Debug/net8.0/AstroGathering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AstroGathering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25329e3c086db06212e51cd58c3833a428c98ec9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c56feecc2ce5adcd979d3fa70cb7cef037d6330")]
 [assembly: System.Reflection.AssemblyProductAttribute("AstroGathering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AstroGathering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
